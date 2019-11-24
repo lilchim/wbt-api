6300a93e39f17aa7b1dfdeeb9716780564f19cdd
lilchim <lilchim@protonmail.com>
2019-11-24 00:11:25 -0600

    Pulls Port from env

--- index.ts
+++ index.ts
@@ -73,6 +73,8 @@ const server = new ApolloServer( {
     }
 })
 
-server.listen().then(({ url }) => {
+server.listen({
+    port: process.env.PORT || 4000
+}).then(({ url }) => {
     console.log(`🚀 WBT Server running on ${url}`);
   });
