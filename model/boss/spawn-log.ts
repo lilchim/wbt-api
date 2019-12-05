f9b5f420f7d8293cca19d29822a443608e9d82d8
lilchim <lilchim@protonmail.com>
2019-12-05 10:47:27 -0600

    Spawn Log Model
    # Adds a function to get the most current boss spawn log.
    # Updates logKill to take an instanceId

--- model/boss/spawn-log.ts
+++ model/boss/spawn-log.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export interface SpawnLog {
     _id: any;
+    instanceId: any;
     bossId: any;
     spawnedAt: number;
     killedAt?: number;
@@ -14,10 +15,25 @@ export const generateSpawnLogModel = ({req, client}) => ({
     getAll: async () => { 
         return client.db('wbt-data').collection('spawn-logs').find().toArray();
     },
-    logSpawn: async ({bossId, spawnTime}) => { 
+    getActiveLog: async (bossId) => {
+        console.log('finding current log for boss with id ', bossId);
+        let activeLog = await client.db('wbt-data').collection('scouting-logs')
+            .find(
+                {
+                    target: bossId,
+                }
+            )
+            .sort(
+                { spawnedAt: 1 }
+            ).toArray()
+
+        console.log('current spawn instance: ', activeLog);
+    },
+    logSpawn: async ({bossId, spawnTime, instanceId}) => { 
         // Log the boss spawn
         let log: SpawnLog = {
             _id: uuidv4(),
+            instanceId: instanceId ? instanceId : -1,
             bossId: bossId,
             spawnedAt: spawnTime ? spawnTime : Math.floor(Date.now() / 1000),
         }
@@ -26,10 +42,13 @@ export const generateSpawnLogModel = ({req, client}) => ({
         return  log;
 
     },
-    logKill: async ({_id, spawnId, timeOfDeath}) => { 
+    logKill: async ({bossId, instanceId, timeOfDeath}) => { 
         // Update the kill log
         let logResult = await client.db('wbt-data').collection('spawn-logs').findOneAndUpdate(
-            { _id: spawnId },
+            { 
+                target: bossId,
+                instanceId: instanceId ? instanceId : -1
+            },
             { $set: { killedAt: timeOfDeath } },
             { upsert: true }
         )
